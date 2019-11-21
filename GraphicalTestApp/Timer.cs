8eff83e4c24fada57f04e2fa8f24007ac88ce0f4
Van Phelan <van.phelan@aie.edu>
2019-11-21 12:33:37 -0600

    Added comments to Timer

--- GraphicalTestApp/Timer.cs
+++ GraphicalTestApp/Timer.cs
@@ -12,22 +12,25 @@ namespace GraphicalTestApp
 
         private float _deltaTime = 0.005f;
 
+        //Creates a new Timer and starts it ticking
         public Timer()
         {
             _stopwatch.Start();
         }
 
+        //Reset the time to 0 and continue ticking
         public void Restart()
         {
             _stopwatch.Restart();
         }
 
-        //The time in seconds
+        //The time in seconds that have ellapsed
         public float Seconds
         {
             get { return _stopwatch.ElapsedMilliseconds / 1000.0f; }
         }
 
+        //The time in seonds that have elapsed since the last GetDeltaTime() call
         public float GetDeltaTime()
         {
             _previousTime = _currentTime;
